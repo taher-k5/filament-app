641aaa487f1e937c098b920132752130d9306ebf
taher-k5 <kathiriyataher16@gmail.com>
2024-06-03 20:46:05 +0530

    add filament relationship

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -31,9 +31,9 @@ class StateResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('country_id')
-                    ->required()
-                    ->numeric(),
+                Forms\Components\Select::make('country_id')
+                    ->relationship(name: 'country', titleAttribute: 'name')
+                    ->required(),
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
